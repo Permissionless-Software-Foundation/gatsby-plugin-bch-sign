790259468106f9eb70712970dcc167acd671e30a
Daniel Gonzalez <danielhumgon@gmail.com>
2021-02-08 16:09:57 -0400

    feat(plugin): Convert 'Sign Message' feature into a Gatsby Plugin

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+  This index file is really just a placeholder. It imports and exports the
+  primary component for this plugin.
+*/
+
+import SignMessage from './src/components/sign-message'
+
+export default SignMessage
